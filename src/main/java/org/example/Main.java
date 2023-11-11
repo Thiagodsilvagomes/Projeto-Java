94553d1b517c3edf15c39f0d08d07933d50d6df0
Thiago Silva <titusilvs91@gmail.com>
2023-11-11 00:36:21 -0300

    Adiciona recurso na outra-feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Biblioteca biblioteca = new Biblioteca();
 
         Livro livro1 = new Livro("Dom Casmurro", "Machado de Assis");
-        Livro livro2 = new Livro("Memórias Póstumas de Brás Cubas", "Machado de Assis");
+        Livro livro2 = new Livro("O alienista", "Machado de Assis");
 
         biblioteca.addLivro(livro1);
         biblioteca.addLivro(livro2);
