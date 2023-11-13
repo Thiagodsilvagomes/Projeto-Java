811224d3c9cefa1934d6fb84b0569754dbf58775
Thiago Da silva Gpmes <103701494+Thiagodsilvagomes@users.noreply.github.com>
2023-11-12 22:00:42 -0300

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import Biblioteca.Biblioteca;
+import Livro.Livro;
+
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        Livro livro1 = new Livro("Dom Casmurro", "Machado de Assis");
+        Livro livro2 = new Livro("O alienista", "Machado de Assis");
+
+        biblioteca.addLivro(livro1);
+        biblioteca.addLivro(livro2);
+
+        biblioteca.listarLivros();
+
+    }
+}
